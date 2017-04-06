c78002114d51fae2e2bf5dfa1309051594571a7b
golubovicana <ana.golubovic37@gmail.com>
2017-04-06 17:38:04 +0200

    Implementirane metode interfejsa InterfejsZaMenjacnicu u okviru klase Menjacnica

---
+++ Menjacnica/src/m/Menjacnica.java
@@ -0,0 +1,29 @@
+package m;
+
+import java.util.GregorianCalendar;
+
+import interfejs.interfejsZaMenjacnicu;
+import valute.Valuta;
+
+public class Menjacnica implements interfejsZaMenjacnicu{
+
+	
+	public void dodajNoviKurs(GregorianCalendar datum, String naziv, String skraceniNaziv, double prodajni,
+			double srednji, double kupovni) {
+		
+		
+	}
+
+	
+	public void obrisiKurs(GregorianCalendar datum, String naziv) {
+		
+		
+	}
+
+	
+	public Valuta pronadjiKurs(GregorianCalendar datum, String naziv) {
+		
+		return null;
+	}
+	
+}
